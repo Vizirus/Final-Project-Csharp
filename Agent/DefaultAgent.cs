5e19bb2d2de9900878baefa4e450b9c0fbda74c4
artem <denloken@gmail.com>
2025-06-05 21:32:57 +0300

    Added Agent behaivour and test

--- Agent/DefaultAgent.cs
+++ Agent/DefaultAgent.cs
@@ -2,6 +2,63 @@
 {
     public class DefaultAgent
     {
+        private string _filePath { get; set; }
+        private string[] _fileToString { get; set; }    
+        private Dictionary<string, int> _fileContent { get; set; }
+        public DefaultAgent(string filePath)
+        {
+            this._filePath = filePath;
+            this._fileContent = new Dictionary<string, int>();
+        }
 
+        public void ChangeFilePath(string newPath)
+        {
+            this._filePath = newPath;
+        }
+        public async Task ParseFileAsync()
+        {
+            string buffer = string.Empty;
+            using (StreamReader fileStream = new StreamReader(_filePath))
+            {
+                buffer = await fileStream.ReadToEndAsync();
+                _fileToString = buffer.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                fileStream.Close();
+            }
+        }
+        public void CalculateWordFrequency(string searchedWord) //MUST be Tasked
+        {
+            if (string.IsNullOrEmpty(searchedWord))
+            {
+                throw new ArgumentException("Searched word cannot be null or empty.");
+            }
+            else
+            {
+                int count = _fileToString.Count(x => x == searchedWord);
+                if(_fileContent.ContainsKey(searchedWord))
+                {
+                    _fileContent[searchedWord] += count;
+                }
+                else
+                {
+                    _fileContent.Add(searchedWord, count);
+                }
+            }
+                
+        }
+        public (string, int) GetContentByToken(string token)
+        {
+            string returnTok = string.Empty;
+            int amount = 0;
+            if (_fileContent.ContainsKey(token))
+            {
+                returnTok = token;
+                amount = _fileContent[token];
+            }
+            else
+            {
+                amount = 0;
+            }
+            return (returnTok, amount);
+        }
     }
 }
---
+++ AgentTest/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Agent;
+using Xunit;
+namespace AgentTest
+{
+    public class UnitTest1
+    {
+        private void createTheFile(string filename)
+        {
+            using (var writer = new System.IO.StreamWriter(filename))
+            {
+                writer.WriteLine("This is a test file for the DefaultAgent.");
+                writer.WriteLine("It contains some sample test test text to be processed by the agent.");
+                writer.Close();
+            }
+        }
+        [Fact]
+        public async void Test1()
+        {
+            createTheFile("testFile.txt");
+            DefaultAgent agent = new DefaultAgent("testFile.txt");
+            await agent.ParseFileAsync();
+
+            //Try to find all 'text' word in the given test
+            agent.CalculateWordFrequency("text");
+            var result = agent.GetContentByToken("text");
+
+            Assert.Equal(1, result.Item2);
+        }
+        [Fact]
+        public async void TestOneMoreFile()
+        {
+            createTheFile("testFile2.txt");
+            DefaultAgent agent = new DefaultAgent("testFile2.txt");
+            await agent.ParseFileAsync();
+
+            //Try to find all 'test' word in the given test
+            agent.CalculateWordFrequency("test");
+            var result = agent.GetContentByToken("test");
+
+            Assert.Equal(3, result.Item2);
+        }
+        [Fact]
+        public async void DynamicChange()
+        {
+            DefaultAgent agent = new DefaultAgent("testFile.txt");
+            await agent.ParseFileAsync();
+
+            //Try to find all 'text' word in the given test
+            agent.CalculateWordFrequency("text");
+            var result = agent.GetContentByToken("text");
+
+            Assert.Equal(1, result.Item2);
+
+            //Try to find 'test' word in a second file
+            agent.ChangeFilePath("testFile2.txt");
+            await agent.ParseFileAsync();
+            agent.CalculateWordFrequency("test");
+
+            result = agent.GetContentByToken("test");
+            Assert.Equal(3, result.Item2);
+        }
+        [Fact]
+        public async void ReadFromLorenIpsum()
+        {
+            DefaultAgent agent = new DefaultAgent("lorenI.txt");
+            await agent.ParseFileAsync();
+            agent.CalculateWordFrequency("Lorem");
+            var result = agent.GetContentByToken("Lorem");
+
+            Assert.Equal(1, result.Item2);
+        }
+        [Fact]
+        public async void ReadZero()
+        {
+            DefaultAgent agent = new DefaultAgent("lorenI.txt");
+            await agent.ParseFileAsync();
+            agent.CalculateWordFrequency("UnknowWordWhich");
+            var result = agent.GetContentByToken("UnknowWordWhich");
+
+            Assert.Equal(0, result.Item2);
+        }
+        [Fact]
+        public async void AccessWrongToken()
+        {
+            DefaultAgent agent = new DefaultAgent("lorenI.txt");
+            await agent.ParseFileAsync();
+            agent.CalculateWordFrequency("Lorem");
+            var result = agent.GetContentByToken("UnknowWordWhich");
+
+            Assert.Equal(0, result.Item2);
+            Assert.Equal(string.Empty, result.Item1);
+        }
+    }
+}
